c6fe08fe3a84ae363d5320cdb74cd24e99c64cb2
JeremyDelorme <jeremydelorme23.7@gmail.com>
2022-03-23 23:41:35 +0100

    Updates

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,11 +8,7 @@ export class MainView extends React.Component {
     constructor() {
         super();
         this.state = {
-            movies: [
-                { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...' },
-                { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...' },
-                { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...' }
-            ],
+            movies: [],
             selectedMovie: null
         };
     }
@@ -38,14 +34,14 @@ export class MainView extends React.Component {
     render() {
         const { movies, selectedMovie } = this.state;
 
-        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+        if (movies.length === 0) return <div className="main-view" />;
 
         return (
             <div className="main-view">
                 {selectedMovie
                     ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
                     : movies.map(movie => (
-                        <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+                        <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
                     ))
                 }
             </div>
