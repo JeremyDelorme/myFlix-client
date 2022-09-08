c2a78f765c6fcb26bc1de43b735ee627be674b35
JeremyDelorme <jeremydelorme23.7@gmail.com>
2022-09-08 13:43:50 -0400

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import axios from 'axios';
-
 import { connect } from 'react-redux';
-
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import { setMovies } from '../../actions/actions';
 import MoviesList from '../movies-list/movies-list';
-
 import { Container, Col, Row } from 'react-bootstrap';
 import "./main-view.scss"
-
 import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
 import { MovieView } from '../movie-view/movie-view';
@@ -18,16 +13,13 @@ import { NavbarView } from '../navbar-view/navbar-view';
 import { ProfileView } from '../profile-view/profile-view';
 import { GenreView } from '../genre-view/genre-view';
 import { DirectorView } from '../director-view/director-view';
-
 class MainView extends React.Component {
-
     constructor() {
         super();
         this.state = {
             user: null
         };
     }
-
     componentDidMount() {
         let accessToken = localStorage.getItem('token');
         if (accessToken !== null) {
@@ -37,7 +29,6 @@ class MainView extends React.Component {
             this.getMovies(accessToken);
         }
     }
-
     getMovies(token) {
         axios.get('https://movie-api-jeremydelorme.herokuapp.com/movies', {
             headers: { Authorization: `Bearer ${token}` }
@@ -49,18 +40,15 @@ class MainView extends React.Component {
                 console.log(error);
             });
     }
-
     onLoggedIn(authData) {
         console.log(authData);
         this.setState({
             user: authData.user.Username
         });
-
         localStorage.setItem('token', authData.token);
         localStorage.setItem('user', authData.user.Username);
         this.getMovies(authData.token);
     }
-
     onLoggedOut() {
         localStorage.removeItem('token');
         localStorage.removeItem('user');
@@ -68,13 +56,9 @@ class MainView extends React.Component {
             user: null
         });
     }
-
-
-
     render() {
         let { movies } = this.props;
         let { user } = this.state;
-
         return (
             <Router>
                 <NavbarView user={user} />
@@ -94,7 +78,6 @@ class MainView extends React.Component {
                                 </Col>
                             )
                         }} />
-
                         <Route path="/movies/:movieId" render={({ match, history }) => {
                             if (movies.length === 0) return <div />;
                             return (
@@ -103,7 +86,6 @@ class MainView extends React.Component {
                                 </Col>
                             )
                         }} />
-
                         <Route path={"/users/:userId"} render={({ history }) => {
                             return (
                                 <Col >
@@ -111,7 +93,6 @@ class MainView extends React.Component {
                                 </Col>
                             )
                         }} />
-
                         <Route path={"/directors/:name"} render={({ match, history }) => {
                             if (!user) return <Redirect to="/" />
                             // If movie list is empty (while movies load from API), display empty page
@@ -122,7 +103,6 @@ class MainView extends React.Component {
                                 </Col>
                             )
                         }} />
-
                         <Route path={"/genres/:name"} render={({ match, history }) => {
                             if (!user) return <Redirect to="/" />
                             // If movie list is empty (while movies load from API), display empty page
@@ -139,9 +119,7 @@ class MainView extends React.Component {
         );
     }
 }
-
 let mapStateToProps = state => {
     return { movies: state.movies }
 }
-
 export default connect(mapStateToProps, { setMovies })(MainView);
